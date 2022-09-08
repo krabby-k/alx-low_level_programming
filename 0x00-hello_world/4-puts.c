7483607aff818ddc3b2d97b52cfd75823fa933e9
krabby-k <briankin86@gmail.com>
2022-09-08 13:40:39 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+	/* string initialization */
+	char mystr[] = "\"Programming is like building a multilingual puzzle";
+	puts(mystr); /* writing to stdout */
+	return 0;
+}
