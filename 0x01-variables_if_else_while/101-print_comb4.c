9940226d3130b3bdb51289443b25f91cc9dd4923
krabby-k <briankin86@gmail.com>
2022-09-11 14:53:29 -0700

    Advanced 101

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,23 +11,23 @@ int main(void)
 {
 	int x, y, z;
 
-	for (x = 48; x <= 56; x++)
+	for (x = 48; x < 56; x++)
 	{
-		for (y = 49; y <= 57; y++)
+		for (y = 49; y < 57; y++)
 		{
-			for (z = 50; z <= 58; z++)
+			for (z = 50; z < 58; z++)
 			{
 				if (z > y && y > x)
 				{
 					putchar(x);
 					putchar(y);
 					putchar(z);
-				
 					if (x != 55 || y != 56)
 					{
 						putchar(',');
 						putchar(' ');
 					}
+
 				}
 			}
 		}
