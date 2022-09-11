c3a6a7a8f1d9a2f738e673637420982d0749628b
krabby-k <briankin86@gmail.com>
2022-09-11 14:38:28 -0700

    Advanced 101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Description: using main function & putchar
+ * prints numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int x, y, z;
+
+	for (x = 48; x <= 56; x++)
+	{
+		for (y = 49; y <= 57; y++)
+		{
+			for (z = 50; z <= 58; z++)
+			{
+				if (z > y && y > x)
+				{
+					putchar(x);
+					putchar(y);
+					putchar(z);
+				}
+				if (x != 55 && y != 56)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
