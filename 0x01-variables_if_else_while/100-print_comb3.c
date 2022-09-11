5d007d5dfc17fcd9279cd5fde3c646174f970aa6
krabby-k <briankin86@gmail.com>
2022-09-11 14:18:41 -0700

    Advanced 100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,13 +11,13 @@ int main(void)
 {
 	int x, y;
 
-	for (x = 0; x <= 8; x++)
+	for (x = 48; x <= 56; x++)
 	{
 		for (y = 49; y <= 57; y++)
 		{
 			if (y > x)
 			{
-				putchar((x % 10) + '0');
+				putchar(x);
 				putchar(y);
 				if (x != 56 || y != 57)
 				{
