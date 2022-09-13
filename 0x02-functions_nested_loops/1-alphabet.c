08b4f4c420f5026c921c2fad19401ecbbedf6ba4
krabby-k <briankin86@gmail.com>
2022-09-13 14:52:41 -0700

    lowecase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * description prints the alphabet, in lowercase
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+}
