c49f2ec9420dd788d27c8a31ff100fce60afd339
krabby-k <briankin86@gmail.com>
2022-09-15 13:29:02 -0700

    digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for uppercase character
+ * @c: arg supplied
+ * Returns: 1 if c is digit
+ * Return: 0
+ */
+
+int _isupper(int c)
+{
+	if ((c >= 0) && (c <= 9))
+	{
+		return (1);
+	}
+	return (0);
+}
