fce2ea742d474d78d504aec33b30d8b494f665ee
krabby-k <briankin86@gmail.com>
2022-09-15 15:12:46 -0700

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,13 +10,16 @@ void more_numbers(void)
 {
 	int x, y;
 	
-	for (x = 0; x <= '9'; x++)
+	for (x = 0; x <= 9; x++)
 	{
-		for (y = 48; y <= 62; x++)
+		for (y = 0; y <= 14; y++)
 		{
-			putchar(y);
+			if (y > 9)
+			{
+				putchar((y / 10) + '0');
+			}
+			putchar((y % 10) + '0');
 		}
 		putchar('\n');
 	}
-	putchar('\n');
 }
