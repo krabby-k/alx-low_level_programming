df1e51265b04a64d3b3df14fa2c98e5f943770a1
krabby-k <briankin86@gmail.com>
2022-09-15 07:26:28 -0700

    main

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,4 +4,8 @@
 #include <stdio.h>
 
 void positive_or_negative(int x);
+int largest_number(int, int, int);
+void print_remaining_days(int, int, int);
+int convert_day(int,int);
+
 #endif /* MAIN_H */
