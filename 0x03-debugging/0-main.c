2e2213afadbfe6094ffe7de3046a66a01047b4aa
krabby-k <briankin86@gmail.com>
2022-09-15 07:18:38 -0700

    +ve or -ve

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - tests function that prints if int is +ve or -ve
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+
+	x = 0;
+	positive_or_negative(x);
+
+	return (0);
+}
