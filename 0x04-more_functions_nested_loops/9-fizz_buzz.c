ff8c192f7c9ee35a6eb31cd8c91de57bec21297c
krabby-k <briankin86@gmail.com>
2022-09-16 13:09:34 -0700

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -4,31 +4,31 @@
  * main -entry point
  *
  * print numbers
- * Return: nothing
+ * Return: 0
  */
-void main(void)
+int main(void)
 {
 	int x = 0;
 
 	while (x++ <= 100)
 	{
-		if ((x % 3) == 0 && (x % 5) == 0)
+		if (x % 3 == 0 && x % 5 == 0)
 		{
 			printf("FizzBuzz ");
 		}
-		else if ((x % 3) == 0)
+		else if (x % 3 == 0)
 		{
-			printf("Buzz ");
+			printf("Fizz ");
 		}
-		else if ((x % 5) == 0)
+		else if (x % 5 == 0)
 		{
 			if ( x == 100)
 			{
 				printf("Buzz ");
-				printf('\n');
+				printf("\n");
 			}
 			else
-				printf("Buzz");
+				printf("Buzz ");
 		}
 		else
 		{
