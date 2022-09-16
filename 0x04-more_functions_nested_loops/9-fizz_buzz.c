d7578f43f322b4a5377c98e39268e89cb1f9a254
krabby-k <briankin86@gmail.com>
2022-09-16 14:02:14 -0700

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,11 +14,11 @@ int main(void)
 	{
 		if (x % 3 == 0 && x % 5 == 0)
 		{
-			printf("FizzBuzz ")
+			printf("FizzBuzz ");
 		}
 		else if (x % 3 == 0)
 		{
-			printf("Fizz");
+			printf("Fizz ");
 		}
 		else if (x % 5 == 0)
 		{
