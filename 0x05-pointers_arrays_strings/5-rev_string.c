7972991d8b206352646bf86d8c4b3b157d155072
krabby-k <briankin86@gmail.com>
2022-09-20 14:30:09 -0700

    reverse

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -12,9 +12,9 @@ void rev_string(char *s)
 
 	for (i = 0; i < len / 2; i++)
 	{
-		j = *s[i];
-		*s[i] = *s[len -i -1];
-		*s[len -i -1] = j;
+		j = *(s + i);
+		*(s + i) = *s(len - i - 1);
+		*(s + len - i - 1) = j;
 	}
 }
 
@@ -28,7 +28,7 @@ int _strlen(char *s)
 {
 	int x;
 
-	while (*s[x] !='\0')
+	while (*(s +x) != '\0')
 	       x++;
 
 	return (x);
