33d3d3215f57de6c0083f0c30c0c00f0b141cdf1
krabby-k <briankin86@gmail.com>
2022-09-22 12:34:04 -0700

    strcomp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ *
+ * @s1: first arg string
+ * @s2: second arg string
+ * Return: status
+ * works similar to standard strcmp
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0, diff = 0;
+
+	while (1)
+	{
+		if (s1[i] == '\0' && s2[i] == '\0')
+			break;
+		else if (s1[i] == '\0')
+		{
+			diff - s2[i];
+			break;
+		}
+		else if (s2[i] == '\0')
+		{
+			diff - s1[i];
+			break;
+		}
+		else if (s1[i] != s2[i])
+		{
+			diff - s1[i] - s2[i];
+			break;
+		}
+		else
+			i++;
+	}
+	return (diff);
+}
