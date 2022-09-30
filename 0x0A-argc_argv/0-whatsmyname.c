40b936e4050f52ca557b50d5c3a10cd1f089a483
krabby-k <briankin86@gmail.com>
2022-09-30 14:36:31 -0700

    argc argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - prints name of the program
+ *
+ * Retun:0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	(void)argc;
+	exit(EXIT_SUCCESS);
+}
