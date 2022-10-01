39cfb1eaf5efde2f0bcddfa961f42776d9d32819
krabby-k <briankin86@gmail.com>
2022-10-01 15:43:04 -0700

    add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * main - prints all arguments it receives
+ * @argc: number of args
+ * @argv: array of args
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int a = 0, i, j;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j]; j++)
+		{
+			if (isdigit(argv[i][j]) == 0)
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+	}
+	for (i = 1; i < argc; i++)
+	{
+		a += atoi(argv[i]);
+	}
+	printf("%d\n", a);
+	return (0);
+}
