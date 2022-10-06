108cdaa440af796d4956252b57ec198493dc057b
krabby-k <briankin86@gmail.com>
2022-10-06 15:05:12 -0700

    allocate mem

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,6 +12,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *p;
 	unsigned int i;
+
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	p = malloc(nmemb * size);
@@ -20,4 +21,4 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	for (i = 0; i < (nmemb * size); i++)
 		p[i] = 0;
 	return (p);
-}	
+}
