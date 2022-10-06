ac5e68f41ee2c2105741d22fc69ad35a50e1407e
krabby-k <briankin86@gmail.com>
2022-10-06 15:00:21 -0700

    allocate mem

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _calloc - allocate memory of an array using malloc
+ *
+ * @nmemb: number of elements in arry
+ * @size: void pinter to be allocated
+ *
+ * Return: void pinter
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	cahr *p;
+	unsigned int i;
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	p = malloc(nmemb * size);
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < (nmemb * size); i++)
+		p[i] = 0;
+	return (p);
+}	
