e1ee8445f937ea41eb1f070536316cb19b952f31
krabby-k <briankin86@gmail.com>
2022-10-11 14:18:56 -0700

    a

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,14 +8,15 @@
  * @age: second member
  * @owner: third member
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}dog_t;
+} dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-#endif /*DOG_H*/
+#endif
