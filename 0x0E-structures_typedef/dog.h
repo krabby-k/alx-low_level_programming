3b7641c7cde9d2e56a6f4fd6fb9660e84110a5f7
krabby-k <briankin86@gmail.com>
2022-10-11 14:02:56 -0700

    a

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,11 +13,5 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
-typedef struct dog dog_t;
-int _putchar(char);
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
+};
 #endif /*DOG_H*/
