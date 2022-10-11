8a1f65ed7c71ec26112e03ec26f5817887ad46f8
krabby-k <briankin86@gmail.com>
2022-10-11 12:56:55 -0700

    al

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+/**
+ * struct dog - struct dog with 3 elements
+ *
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
