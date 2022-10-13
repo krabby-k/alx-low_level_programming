41eb6673c7779c396f9476efcb311b4b037bfd3d
krabby-k <briankin86@gmail.com>
2022-10-13 15:38:04 -0700

    4

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,10 @@
 #include "function_pointers.h"
 
 /**
- * * print_name - prints a name
- * * @name:name to print
- * * @f: function which print name
- * */
+ * print_name - prints a name
+ * @name:name to print
+ * @f: function which print name
+ */
 
 void print_name(char *name, void (*f)(char *))
 {
